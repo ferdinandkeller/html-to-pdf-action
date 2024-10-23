845700b803fa658e8be3c679af81bd7c68b208c8
Ferdinand Keller <ferdinand.keller@proton.me>
2024-10-23 22:38:11 +0200

    Feature/v2rc (#2)
    * added prettier config
    
    * bumped version
    
    * improved README
    
    * upgraded all the chromium arguments
    
    * improved README

--- generate_pdf.js
+++ generate_pdf.js
@@ -42,7 +42,10 @@ if (!('destination-path' in args)) {
 // 'file:///path/to/root/style.css' to make it work
 // to avoid this, we start a server and serve the files from there, and then we can use the url 'http://127.0.0.1'
 let server = hs.createServer({
-    root: path.join(process.env.GITHUB_WORKSPACE || __dirname, args['source-path']),
+    root: path.join(
+        process.env.GITHUB_WORKSPACE || __dirname,
+        args['source-path']
+    ),
     cache: -1,
 })
 server.listen(8000, '0.0.0.0', generate_pdf)
@@ -52,39 +55,80 @@ async function generate_pdf() {
     // launch the browser
     const browser = await puppeteer.launch({
         args: [
-            // because we are using github action's docker container,
-            // we need to run as root (we can't change that)
-            // chromium throws security warning when running as root, so we need to disable the sandbox
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-gpu',
-            // we also need to disable the shared memory, as we are running in a docker container
-            // with a limited amount of memory
-            '--disable-dev-shm-usage',
-            // absolutely needed, else you will see font kerning problems in the pdf
-            '--font-render-hinting=none',
-            // we want to use the srgb color profile, as this is the default color profile for the web
-            // without that, we will see color differences between the website and the pdf
-            '--force-color-profile=srgb',
-            // because we are generating a pdf, we don't need any extensions
-            '--disable-extensions',
-            // we are running inside docker, we don't need a window
+            // we are running inside docker, we don't want a window
             '--headless',
+            // we provide a default (virtual) window size
+            // that way it's consistent across different environments
+            '--window-size=1920,1080',
+            // we want the window at the top left corner of the (virtual) screen
+            '--window-position=0,0',
+
+            // when starting the browser for the first time, we don't want to see a welcome page
+            // we want to directly go to the website
+            '--no-first-run',
+            // we don't want to see scrollbars
+            '--hide-scrollbars',
+            // we don't want popup saying "chromium is being controlled by automated software"
+            '--disable-infobars',
+            // we don't want to see any notifications, there is no one to interact with them
+            '--disable-notifications',
+
+            // because we are generating a pdf, we don't need any extensions
+            // there shouldn't be any anyway, but just to be sure
+            '--disable-extensions',
+
+            // we are inside a docker container, where sandboxing is broken
+            // we (likely) don't need the sandbox anyway
+            '--no-sandbox',
+            // absolutely required with our execution environment, else chromium will not start
+            '--disable-setuid-sandbox',
+
+            // we also need to disable the shared memory
+            // our container has a very limited amount of memory
+            '--disable-dev-shm-usage',
+            // we won't be using the audio, so we can mute it
+            '--mute-audio',
+            // we don't have a GPU in the docker container
+            '--disable-gpu',
+            // we don't have access to hardware acceleration
+            '--disable-accelerated-2d-canvas',
             // we don't want chromium to run any background tasks,
             // we don't need them and it might slow down the process
             '--disable-background-networking',
             '--disable-component-extensions-with-background-pages',
             '--dns-prefetch-disable',
-            // we don't need scrollbars
-            '--hide-scrollbars',
+            // we don't care about certificate errors that could hinder the process
+            '--ignore-certificate-errors',
+            // we want that to also apply to the skip list
+            '--ignore-certificate-errors-skip-list',
+            // chromium can throttle background pages, we don't want that
+            '--disable-background-timer-throttling',
+            // chromium can limit the resources of background pages, we don't want that
+            '--disable-renderer-backgrounding',
+            // chromium can also stop rendering when the page is occluded, we don't want that
+            '--disable-backgrounding-occluded-windows',
+            // in case of crash, we don't want to send a crash report
+            '--disable-breakpad',
+            // no translation is wanted
+            '--disable-features=TranslateUI,BlinkGenPropertyTrees,IsolateOrigins,site-per-process',
+
+            // we want to use the srgb color profile, as this is the default color profile for the web
+            // without that, we will see color differences between the website and the pdf
+            '--force-color-profile=srgb',
+            // we don't want the browser to scale the website
+            '--force-device-scale-factor=1',
+            // absolutely needed, else you will see font kerning problems in the pdf
+            '--font-render-hinting=none',
         ],
     })
 
     // create a page
     const page = await browser.newPage()
 
-    // set a porper user agent to act like a normal browser
-    await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36");
+    // set a proper user agent to act like a normal browser
+    await page.setUserAgent(
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36'
+    )
 
     // go to the website
     await page.goto('http://localhost:8000', {
@@ -94,7 +138,10 @@ async function generate_pdf() {
 
     // render the page to a pdf
     await page.pdf({
-        path: path.join(process.env.GITHUB_WORKSPACE || __dirname, args['destination-path']),
+        path: path.join(
+            process.env.GITHUB_WORKSPACE || __dirname,
+            args['destination-path']
+        ),
         format: 'A4',
         printBackground: true,
     })
